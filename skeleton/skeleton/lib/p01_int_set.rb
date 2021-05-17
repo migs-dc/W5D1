56786be318da1d64fcb2bd6c8beac99dd6be04c4
GeniusSniper <tiwei61342@gmail.com>
2021-05-17 13:00:41 -0500

    int_set

---
+++ skeleton/skeleton/lib/p01_int_set.rb
@@ -0,0 +1,87 @@
+class MaxIntSet
+  attr_reader :store
+  def initialize(max)
+    @max = max
+    @store = Array.new(max) {false}
+  end
+
+  def insert(num)
+    num < @max && num >= 0 ? @store[num] = true : (raise 'Out of bounds')
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  attr_reader :store
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    i = num % 20
+    @store[i] << num
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
